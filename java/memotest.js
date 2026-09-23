655b4f2bf85dfb4421b0ffc2a1d4c631e87e6fac
miliperuzotti <miliperuzotti@gmail.com>
2026-09-22 23:39:58 -0300

    pantalla tematica

--- java/memotest.js
+++ java/memotest.js
@@ -6,12 +6,15 @@ const btnMultijugador = document.querySelector("#btn-multijugador");
 const pantallaModo = document.querySelector("#pantalla-modo");
 const pantallaJugadores = document.querySelector("#pantalla-jugadores");
 const pantallaTematica = document.querySelector("#pantalla-tematica");
+const pantallaDificultad = document.querySelector("#pantalla-dificultad");
 
 const nombreJugador1 = document.querySelector("#nombre-jugador1");
 const nombreJugador2 = document.querySelector("#nombre-jugador2");
 
 const btnNombres = document.querySelector("#btn-nombres");
 
+const btnCultura = document.querySelector("#btn-cultura");
+const btnAlbumes = document.querySelector("#btn-albumes");
 
 
 // Variables que guardan las decisiones de la partida
@@ -21,6 +24,7 @@ let modoJuego = "";
 let nombre1 = "";
 let nombre2 = "";
 
+let tematica = "";
 
 
 // Cuando se hace click en "Solo"
@@ -64,5 +68,29 @@ btnNombres.addEventListener("click", function() {
         alert("Ingresen los nombres de los dos jugadores.");
 
     }
+
+});
+
+
+// Cuando se hace click en "Cultura general"
+
+btnCultura.addEventListener("click", function() {
+
+    tematica = "culturageneral";
+
+    pantallaTematica.style.display = "none";
+    pantallaDificultad.style.display = "flex";
+
+});
+
+
+// Cuando se hace click en "Álbumes"
+
+btnAlbumes.addEventListener("click", function() {
+
+    tematica = "albumes";
+
+    pantallaTematica.style.display = "none";
+    pantallaDificultad.style.display = "flex";
 
 });
